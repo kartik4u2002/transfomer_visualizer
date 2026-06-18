40a181222b8b8cac28e8823c95a2bd2b99f9df3e
kartik4u2002 <kartik4u2002@gmail.com>
2026-06-18 21:46:03 +0530

    ready for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  // If you are deploying to https://<username>.github.io/<repository-name>/,
+  // uncomment the lines below and replace 'repository-name' with your repository name:
+  // basePath: isProd ? "/repository-name" : undefined,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
